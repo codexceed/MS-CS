bf90fdf21a24e8a131519cbb70d42d48ca965f14
Sarthak Joshi <sarthak.xceed@gmail.com>
2021-10-04 22:54:04 -0400

    WIP: linked.cpp

--- Courses/Operating Systems/linker.cpp
+++ Courses/Operating Systems/linker.cpp
@@ -2,23 +2,15 @@
 
 using namespace std;
 
-void __parseerror(int errcode)
-{
-    static char *errstr[] = {
-        "NUM_EXPECTED",           // Number expect, anything >= 2^30 is not a number either
-        "SYM_EXPECTED",           // Symbol Expected
-        "ADDR_EXPECTED",          // Addressing Expected which is A/E/I/R
-        "SYM_TOO_LONG",           // Symbol Name is too long
-        "TOO_MANY_DEF_IN_MODULE", // > 16
-        "TOO_MANY_USE_IN_MODULE", // > 16
-        "TOO_MANY_INSTR",         // total num_instr exceeds memory size (512) 
-    };
-    printf("Parse Error line %d offset %d: %s\n", linenum, lineoffset, errstr[errcode]);
-}
-
 int main(int argc, char const *argv[])
 {
-    /* code */  
+    if (argc==1){
+        cout<<"Missing Argument: Please pass the path to the linker input file.\n";
+        exit(1);
+    }
+
+    cout<<argv[1];
+
     return 0;
 }
 
